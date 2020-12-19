6155db08720955b15e3d2969faa16371864209aa
* <kaikyandrew235@gmail.com>
2020-12-19 14:28:34 -0300

    Script com ferramentas basicas

---
+++ GV1.py
@@ -0,0 +1,288 @@
+import os, sys 
+os.system("clear")
+
+def banner2():
+	print("""
+⠀⠀⠀⠰⡿⠿⠛⠛⠻⠿⣷
+⠀⠀⠀⠀⠀⠀⣀⣄⡀⠀⠀⠀⠀⢀⣀⣀⣤⣄⣀⡀
+⠀⠀⠀⠀⠀⢸⣿⣿⣷⠀⠀⠀⠀⠛⠛⣿⣿⣿⡛⠿⠷
+⠀⠀⠀⠀⠀⠘⠿⠿⠋⠀⠀⠀⠀⠀⠀⣿⣿⣿⠇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁
+
+⠀⠀⠀⠀⣿⣷⣄⠀⢶⣶⣷⣶⣶⣤⣀
+⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠈⠙⠻⠗
+⠀⠀⠀⣰⣿⣿⣿⠀⠀⠀⠀⢀⣀⣠⣤⣴⣶⡄
+⠀⣠⣾⣿⣿⣿⣥⣶⣶⣿⣿⣿⣿⣿⠿⠿⠛⠃
+⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄
+⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁
+⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁
+⠀⠀⠛⢿⣿⣿⣿⣿⣿⣿⡿⠟
+⠀⠀⠀⠀⠀⠉⠉⠉""")
+
+def banner3():
+	print("""⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄
+⠄⠄⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄
+⠄⢀⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰
+⠄⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗
+⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄
+⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄
+⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄
+⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄
+⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁""")
+
+def banner4():
+	print("""┈┈┈╲┈┈┈┈╱
+┈┈┈╱▔▔▔▔╲
+┈┈┃┈▇┈┈▇┈┃
+╭╮┣━━━━━━┫╭╮
+┃┃┃┈┈┈┈┈┈┃┃┃
+╰╯┃┈┈┈┈┈┈┃╰╯
+┈┈╰┓┏━━┓┏╯
+┈┈┈╰╯┈┈╰╯""")
+
+def banner1():
+	print("""█████████
+█▄█████▄█
+█▼▼▼▼▼
+█
+ GHOST/404 
+█▲▲▲▲▲
+█████████
+██ ██""")
+
+print("｡+ﾟ☆ﾟ+｡★｡+ﾟ☆ﾟ+｡★GV1｡+ﾟ☆ﾟ+｡★｡+ﾟ☆ﾟ+｡")
+print("")
+banner1()
+
+
+def main():
+	teste = input('''
+DIGITE A OPÇAO COM O '0'
+
+[00] PROJETO DE BTF
+[01] PISHING
+[02] SPAM SMS 
+[03] SQLMAP 
+[04] METASPLOIT
+[05] VIRUS
+[06] BOT DO INSTA
+[07] PERSONALIZADOR
+[08] DDOS
+[09] ROUTERSPLOIT
+[10] CANAIS DE TUTORIAIS E PARCEROS 
+[11] CREDITOS
+------> ''')
+	
+	if teste == '01':
+		os.system("clear")
+		print("ISSO PODE DEMORAR UM POUCO")
+		print("")
+		print("Loading…")
+		print("█▒▒▒▒▒▒▒▒▒")
+		print("10%")
+		print("███▒▒▒▒▒▒▒")
+		print("30%")
+		print("█████▒▒▒▒▒")
+		print("50%")
+		print("███████▒▒▒")
+		print("100%")
+		print("██████████")
+		print("")
+		print("FERRAMENTA ------> NgrokCustomLInk")
+		print("")
+		print("╲╭━━━━╮╲╲")
+		print("╲┃╭╮╭╮┃╲╲")
+		print("┗┫┏━━┓┣┛╲")
+		print("╲┃╰━━╯┃")
+		print("╲╰┳━━┳╯╲╲")
+		print("")
+		os.system("git clone https://github.com/doctor154/NgrokCustomLInk")
+		os.system("mv NgrokCustomLInk /data/data/com.termux/files/home")
+		print("")
+		print("FERRAMENTA ------> FotoSploit")
+		print("")
+		os.system("git clone https://github.com/Cesar-Hack-Gray/FotoSploit")
+		os.system("mv FotoSploit /data/data/com.termux/files/home")
+		os.system("rm -rf NgrokCustomLInk")
+		os.system("rm -rf FotoSploit")
+		print("")
+		print("+----------------------+")
+		print("|FERRAMENTAS INSTALADAS|")
+		print("+----------------------+")
+		print("AS FERRAMENTAS SAO ==> NgrokCustomLInk , FOTOSPLOIT")
+	
+	if teste == '02':
+		os.system("clear")
+		print("ISSO PODE DEMORAR UM POUCO")
+		print("")
+		print("Loading…")
+		print("█▒▒▒▒▒▒▒▒▒")
+		print("10%")
+		print("███▒▒▒▒▒▒▒")
+		print("30%")
+		print("█████▒▒▒▒▒")
+		print("50%")
+		print("███████▒▒▒")
+		print("100%")
+		print("██████████")
+		print("")
+		print("FERRAMENTA -------> spamrito")
+		print("")
+		print("_/﹋\_")
+		print("(҂`_´) - TOMA SPAM FDP")
+		print("<;︻╦╤─ ҉ - - - - - - - - - - - - -")
+		print("")
+		os.system("git clone https://github.com/KiritoOfficial/spamrito")
+		os.system("mv spamrito /data/data/com.termux/files/home")
+		print("")
+		print("+---------------------+")
+		print("|FERRAMENTAS ESTALADAS|")
+		print("+---------------------+")
+		print("AS FERRAMENTAS SAO ===> SPAMRITO , ")
+		
+	if teste == '03':
+		os.system("clear")
+		print("ISSO PODE DEMORAR UM POUCO ")
+		print("▬▬▬.◙.▬▬▬")
+		print("═▂▄▄▓▄▄▂")
+		print("◢◤ █▀▀████▄▄▄▄▄▄◢◤")
+		print("█▄ █ :) ██▀▀▀▀▀▀▀╬")
+		print("◥█████◤")
+		print("══╩══╩══")
+		print("▬▬▬.SQLMAP.▬▬▬")
+		print("")
+		print("Loading…")
+		print("█▒▒▒▒▒▒▒▒▒")
+		print("10%")
+		print("███▒▒▒▒▒▒▒")
+		print("30%")
+		print("█████▒▒▒▒▒")
+		print("50%")
+		print("███████▒▒▒")
+		print("100%")
+		print("██████████")
+		print("")
+		os.system("git clone https://github.com/sqlmapproject/sqlmap")
+		os.system("mv sqlmap /data/data/com.termux/files/home")
+		os.system("rm -rf sqlmap")
+		os.system("clear")
+		print("")
+		print("+---------------------+")
+		print("|FERRAMENTAS ESTALADAS|")
+		print("+---------------------+")
+		print("ESSAS SAO AS FERRAMENTAS -----> sqlmap , ")
+		
+	if teste == '04':
+		print("")
+		print("................/´¯/) ")
+		print(".............../¯../ ")
+		print("..METASPLOIT../..../ ")
+		print("........../´¯/'...'/´¯¯`·¸ ")
+		print("......./'/.../..../......./¨¯\ ")
+		print(".....('(...´...´.... ¯~/'...') ")
+		print("......\.................'...../ ")
+		print(".......''...\.......... _.·´ ")
+		print(".........\..............( ")
+		print("...........\.............\...")
+		print("ISSO PODE DEMORAR ")
+		print("")
+		os.system("pkg install unstable-repo")
+		os.system("pkg install metasploit")
+		print("")
+		print("PRONTO AGORA E SO INICIAR COM 'MSFCONSOLE'")
+		
+	if teste == '05':
+		print("")
+		banner4()
+		print("FERRAMENTA ----> Evil-create-framework")
+		print("")
+		os.system("git clone https://github.com/LOoLzeC/Evil-create-framework")
+		os.system("mv Evil-create-framework /data/data/com.termux/files/home")
+		print("")
+		print("FERRAMENTA INSTALADA 'EVIL-CREATE-FRAMEWORK'")
+		
+	if teste == '06':
+		print("")
+		print("ferramenta ---> toolsig.git")
+		os.system("git clone https://github.com/officialputuid/toolsig.git")
+		os.system("mv toolsig.git /data/data/com.termux/files/home")
+		print("")
+		print("FERRAMENTA INSTALADA ")
+		print("")
+		print("video de como usar ela ----> https://youtu.be/l4nvyz3XvwY")
+		
+	if teste == '07':
+		print("")
+		print("ferramenta ----> tstyling")
+		print("")
+		os.system("git clone https://github.com/darkwarrior3/tstyling")
+		os.system("mv tstyling /data/data/com.termux/files/home")
+		print("")
+		print("FERRAMENTA INSTALADA ----> TSTYLING ")
+		print("")
+		print("video esplicando como usar ----> https://youtu.be/jHansTczNMY")
+		
+	if teste == '08':
+		print("ferramentas ----> hammer , torshammer")
+		print("")
+		os.system("git clone https://github.com/liorvh/hammer-1")
+		os.system("git clone https://github.com/dotfighter/torshammer")
+		os.system("mv hammer-1 /data/data/com.termux/files/home")
+		os.system("mv torshammer /data/data/com.termux/files/home")
+		print("")
+		print("FERRAMENTA INSTALADA ---> hammer , torshammer")
+		
+	if teste == '09':
+		print("")
+		print("ferramenta ---> ROUTERSPLOIT")
+		print("")
+		os.system("git clone https://github.com/threat9/routersploit")
+		os.system("mv routersploit /data/data/com.termux/files/home")
+		print("")
+		print("FERRAMNETA INSTALADA ---> ROUTERSPLOIT ")
+		print("Como usar ---> https://youtu.be/_2QSf0Evtjc")
+		
+	if teste > '12':
+		print("")
+		print("+--------------+")
+		print("|opçao invalida|")
+		print("+--------------+")
+		banner1()
+		main()
+		
+	if teste == '10':
+		print("")
+		banner3()
+		print("")
+		print("teddy modsTS")
+		print("https://youtube.com/channel/UCdACtP09kfp97TWm8HgoSMw")
+		
+	if teste == '11':
+		banner2()
+		print("")
+		print("Zap do criador ----> WA.me/+15144168126")
+		print("Discord do criador ---> GHOST/404#5906")
+		print("github de um parceiro ----> https://github.com/luc4sd3v")
+		print("")
+		
+	if teste == '00':
+		print("")
+		key=input("Digite uma palavra de 4 siliba :")
+		
+		L=('a','e', 'i', 'o', 'u','ba','be','bi','bo','bu','ca','ce','ci','co','cu','da','de','di','do','du','fa','fe','fi','fo','fu','ga','ge','gi','go','gu','ha','he','hi','ho','ia','ie','io','iu','hu','ja','je','ji','jo','ju','ka','ke','ki','ko','ku','la','le','li','lo','lu','ma','me','mi','mo','mu','na','ne','ni','no','nu','pa','pe','pi','po','pu','qu','ra','re','ri','ro','ru','sa','se','si','so','su','ta','te','ti','to','tu','va','ve','vi','vo','vu','wa','we','wi','wo','wu','xa','xe','xi','xo','xu','za','ze','zi','zo','zu', 'cha','che','chi','cho','chu','lha', 'lhe', 'lhi','lho', 'lhu', 'nha','nhe','nhi','nho','nhu','rra','rre','rri','rro','rru','ssa','sse','ssi','sso','ssu','qua','que','qui','quo')
+		
+		for i1 in(L):
+			for i2 in(L):
+				for i3 in(L):
+					for i4 in(L):
+						y=(i1+i2+i3+i4)
+						print(y)
+						if(y==key) :
+							print('Encontrada')
+							exit()
+							
+
+if __name__ == "__main__":
+        main()
